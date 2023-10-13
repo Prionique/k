3963e107ab54027086d9c5348a582d9acdd23911
Primon <115495464+Prionique@users.noreply.github.com>
2023-10-14 01:15:13 +0300

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,13 @@
+from sqlite3 import *
 
+con = connect("data.db")
+cur = con.cursor()
+
+a = [("primon", "123")]
+cur.excecute("create table users (login text, pwd text)")
+
+cur.excecute("insert into users values (?,?)", a)
+con.commit()
+
+for i in cur.excecute("select * from users"):
+  print(i)
