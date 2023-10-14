6041091b149f3dd06ff642f145c35837f3cb4738
Primon <115495464+Prionique@users.noreply.github.com>
2023-10-14 13:13:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flet import *
 
 def main(page: Page):
+   page.title("Hello")
    
-
-    page.add(TextField(label = "Login"))
-    page.add(TextButton("Loginn"))
+   page.add(TextField(label = "Login"))
+   page.add(TextButton("Loginn"))
 
 
 app(main, view = WEB_BROWSER)
