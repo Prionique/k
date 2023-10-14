66906ccacafb465d335e73661c10f0372ac23f00
Primon <115495464+Prionique@users.noreply.github.com>
2023-10-14 12:27:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from flet import *
 from sqlite3 import *
 
 def main(page: Page):
-    page.add(Text("Hello World"))
-    page.update()
+
 
     page.add(TextField(label = "Login"))
     page.add(TextButton("Login"))
+
+    page.update
 
 app(main, view = WEB_BROWSER)
