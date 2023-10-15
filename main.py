9d62ce3abc2f578ded97e720f81dc7c2fa1c9a43
Primon <115495464+Prionique@users.noreply.github.com>
2023-10-15 10:47:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ from flet import *
 
 def main(page: Page):
 
-   def lo(_):
-      page.add(Text(f"{a.value}"))
-   a = TextField(label = "Login")
-   page.add(a)
-   page.add(TextButton("Login to the syste
-   ", on_click = lambda : _ lo(_)))
+   page.add(Text("Hello"))
 
 
 app(main, view = WEB_BROWSER)
